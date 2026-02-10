e4b4805e5e28a5e8559da66306ecf0e23ba1acea
SamuelMelian <s.melian.2022@alumnos.urjc.es>
2026-02-10 10:16:45 +0100

    feat: initial project structure and static assets

---
+++ zendashop/src/main/java/es/dawgrupo2/zendashop/ZendashopApplication.java
@@ -0,0 +1,13 @@
+package es.dawgrupo2.zendashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZendashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZendashopApplication.class, args);
+	}
+
+}
---
+++ zendashop/src/test/java/es/dawgrupo2/zendashop/ZendashopApplicationTests.java
@@ -0,0 +1,13 @@
+package es.dawgrupo2.zendashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZendashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
