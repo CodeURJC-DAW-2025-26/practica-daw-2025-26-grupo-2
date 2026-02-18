b28fa790235edefa9444377c02c2e3cdde145571
SamuelMelian <s.melian.2022@alumnos.urjc.es>
2026-02-18 14:36:29 +0100

    feat: add Garment entity model

---
+++ zendashop/src/main/java/es/dawgrupo2/zendashop/model/Garment.java
@@ -0,0 +1,90 @@
+package es.dawgrupo2.zendashop.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Garment {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String name;
+	private Integer price;
+	private String category;
+	private String description;
+    private String features;
+	private String image;
+
+	public Garment() {
+	}
+
+	public Garment(String name, Integer price, String category, String description, String features, String image) {
+		super();
+		this.name = name;
+		this.price = price;
+		this.category = category;
+		this.description = description;
+        this.features = features;
+		this.image = image;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+	public String getFeatures() {
+		return features;
+	}
+
+	public void setFeatures(String features) {
+		this.features = features;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+}
