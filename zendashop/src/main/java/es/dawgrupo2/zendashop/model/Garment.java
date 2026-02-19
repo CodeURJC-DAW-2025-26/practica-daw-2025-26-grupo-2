6324c0952914a07cba2024e00723e4667454b1ae
SamuelMelian <s.melian.2022@alumnos.urjc.es>
2026-02-19 01:20:55 +0100

    feat: create garment model entity and relationships

--- zendashop/src/main/java/es/dawgrupo2/zendashop/model/Garment.java
+++ zendashop/src/main/java/es/dawgrupo2/zendashop/model/Garment.java
@@ -1,9 +1,20 @@
 package es.dawgrupo2.zendashop.model;
 
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Garment {
@@ -13,23 +24,33 @@ public class Garment {
 	private Long id;
 	
 	private String name;
-	private Integer price;
+	private BigDecimal price;
 	private String category;
 	private String description;
     private String features;
-	private String image;
+
+    @Column(updatable = false)
+    private LocalDateTime creationDate = LocalDateTime.now();
+
+    @Lob
+	private Blob image;
+
+    @OneToMany(mappedBy = "garment", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Opinion> opinions = new ArrayList<>();
+
+    @ManyToMany
+    private List<Order> orders = new ArrayList<>();
 
 	public Garment() {
 	}
 
-	public Garment(String name, Integer price, String category, String description, String features, String image) {
+	public Garment(String name, BigDecimal price, String category, String description, String features) {
 		super();
 		this.name = name;
 		this.price = price;
 		this.category = category;
 		this.description = description;
         this.features = features;
-		this.image = image;
 	}
 
 	public Long getId() {
@@ -48,11 +69,11 @@ public class Garment {
         this.name = name;
     }
 
-    public Integer getPrice() {
+    public BigDecimal getPrice() {
         return price;
     }
 
-    public void setPrice(Integer price) {
+    public void setPrice(BigDecimal price) {
         this.price = price;
     }
 
@@ -80,11 +101,41 @@ public class Garment {
 		this.features = features;
 	}
 
-	public String getImage() {
+	public Blob getImage() {
 		return image;
 	}
 
-	public void setImage(String image) {
+	public void setImage(Blob image) {
 		this.image = image;
 	}
+
+    public List<Opinion> getOpinions() {
+		return opinions;
+	}
+
+	public void setOpinions(List<Opinion> opinions) {
+		this.opinions = opinions;
+	}
+
+	public void addOpinion(Opinion opinion) {
+        opinions.add(opinion);
+        opinion.setGarment(this);
+    }
+ 
+    public void removeOpinion(Opinion opinion) {
+        opinions.remove(opinion);
+        opinion.setGarment(null);
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
 }
