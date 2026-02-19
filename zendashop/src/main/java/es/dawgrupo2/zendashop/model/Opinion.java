9021c2a15ab492277cb0ec1ae231446170e66446
SamuelMelian <s.melian.2022@alumnos.urjc.es>
2026-02-19 01:21:04 +0100

    feat: create opinion model entity and relationships

---
+++ zendashop/src/main/java/es/dawgrupo2/zendashop/model/Opinion.java
@@ -0,0 +1,74 @@
+package es.dawgrupo2.zendashop.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Opinion {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private Integer rating;
+    private String comment;
+
+    @ManyToOne
+    private Garment garment;
+
+    @ManyToOne
+    private User user;
+
+	public Opinion() {
+	}
+
+	public Opinion(Integer rating, String comment) {
+		super();
+		this.rating = rating;
+        this.comment = comment;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Garment getGarment() {
+        return garment;
+    }
+
+    public void setGarment(Garment garment) {
+        this.garment = garment;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    
+}
