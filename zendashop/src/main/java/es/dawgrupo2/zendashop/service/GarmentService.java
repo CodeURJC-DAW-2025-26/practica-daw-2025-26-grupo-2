2775fd68b5b9a3a9eaffd8d39bed5004ea0ecda2
SamuelMelian <s.melian.2022@alumnos.urjc.es>
2026-02-19 10:25:29 +0100

    feat: Garment and Opinion services

--- zendashop/src/main/java/es/dawgrupo2/zendashop/service/GarmentService.java
+++ zendashop/src/main/java/es/dawgrupo2/zendashop/service/GarmentService.java
@@ -1,5 +1,57 @@
 package es.dawgrupo2.zendashop.service;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+import javax.sql.rowset.serial.SerialBlob;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import es.dawgrupo2.zendashop.model.Garment;
+import es.dawgrupo2.zendashop.repository.GarmentRepository;
+
+
+@Service
 public class GarmentService {
-    
+
+	@Autowired
+	private GarmentRepository repository;
+
+	public Optional<Garment> findById(long id) {
+		return repository.findById(id);
+	}
+
+	public List<Garment> findById(List<Long> ids){
+		return repository.findAllById(ids);
+	}
+	
+	public boolean exist(long id) {
+		return repository.existsById(id);
+	}
+
+	public List<Garment> findAll() {
+		return repository.findAll();
+	}
+
+    public void save(Garment garment) {
+        repository.save(garment);
+    }
+
+	public void save(Garment garment, MultipartFile imageFile) throws IOException{
+		if(!imageFile.isEmpty()) {
+			try {
+				garment.setImage(new SerialBlob(imageFile.getBytes()));
+			} catch (Exception e) {
+				throw new IOException("Failed to create image blob", e);
+			}
+		}
+		this.save(garment);
+	}
+
+	public void delete(long id) {
+		repository.deleteById(id);
+	}
 }
--- zendashop/src/main/java/es/dawgrupo2/zendashop/service/OpinionService.java
+++ zendashop/src/main/java/es/dawgrupo2/zendashop/service/OpinionService.java
@@ -1,5 +1,42 @@
 package es.dawgrupo2.zendashop.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import es.dawgrupo2.zendashop.model.Opinion;
+import es.dawgrupo2.zendashop.repository.OpinionRepository;
+
+
+@Service
 public class OpinionService {
-    
+
+	@Autowired
+	private OpinionRepository repository;
+
+	public Optional<Opinion> findById(long id) {
+		return repository.findById(id);
+	}
+
+	public List<Opinion> findById(List<Long> ids){
+		return repository.findAllById(ids);
+	}
+	
+	public boolean exist(long id) {
+		return repository.existsById(id);
+	}
+
+	public List<Opinion> findAll() {
+		return repository.findAll();
+	}
+
+	public void save(Opinion opinion) {
+		repository.save(opinion);
+	}
+
+	public void delete(long id) {
+		repository.deleteById(id);
+	}
 }
