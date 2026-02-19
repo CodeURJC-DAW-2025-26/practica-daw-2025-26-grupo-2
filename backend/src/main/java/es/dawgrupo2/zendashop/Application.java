67531378c51dfdeaad9d4f98365fb36c0e3ffd2b
SamuelMelian <s.melian.2022@alumnos.urjc.es>
2026-02-19 11:51:05 +0100

    fix: change main class name from ZendaApplication to Application

--- backend/src/main/java/es/dawgrupo2/zendashop/Application.java
+++ backend/src/main/java/es/dawgrupo2/zendashop/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZendashopApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZendashopApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
