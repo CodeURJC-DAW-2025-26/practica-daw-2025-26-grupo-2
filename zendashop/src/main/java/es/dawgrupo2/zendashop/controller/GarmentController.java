cbac9a4915e06865278883bf037539dd9c6f5142
SamuelMelian <s.melian.2022@alumnos.urjc.es>
2026-02-19 01:35:35 +0100

    feat: add Model Attribute in Garment Controller

--- zendashop/src/main/java/es/dawgrupo2/zendashop/controller/GarmentController.java
+++ zendashop/src/main/java/es/dawgrupo2/zendashop/controller/GarmentController.java
@@ -1,21 +1,40 @@
 package es.dawgrupo2.zendashop.controller;
 
+import java.security.Principal;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import es.dawgrupo2.zendashop.model.Garment;
 import es.dawgrupo2.zendashop.repository.GarmentRepository;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class GarmentController {
 
 	@Autowired
 	private GarmentRepository garmentRepository;
+
+    @ModelAttribute
+	public void addAttributes(Model model, HttpServletRequest request) {
+
+		Principal principal = request.getUserPrincipal();
+
+		if (principal != null) {
+
+			model.addAttribute("logged", true);
+			model.addAttribute("userName", principal.getName());
+			model.addAttribute("admin", request.isUserInRole("ADMIN"));
+
+		} else {
+			model.addAttribute("logged", false);
+		}
+	}
 
 	@GetMapping("/")
 	public String showGarments(Model model) {
