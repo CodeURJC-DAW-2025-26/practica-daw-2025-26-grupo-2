a76c9097d2fbff0310147597a7132f1e74c5dc7a
SamuelMelian <s.melian.2022@alumnos.urjc.es>
2026-02-22 20:22:23 +0100

    feat: implement OrderItemService with CRUD operations

---
+++ backend/src/main/java/es/dawgrupo2/zendashop/service/OrderItemService.java
@@ -0,0 +1,46 @@
+package es.dawgrupo2.zendashop.service;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+import javax.sql.rowset.serial.SerialBlob;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import es.dawgrupo2.zendashop.model.OrderItem;
+import es.dawgrupo2.zendashop.repository.OrderItemRepository;
+
+
+@Service
+public class OrderItemService {
+
+	@Autowired
+	private OrderItemRepository repository;
+
+	public Optional<OrderItem> findById(long id) {
+		return repository.findById(id);
+	}
+
+	public List<OrderItem> findById(List<Long> ids){
+		return repository.findAllById(ids);
+	}
+	
+	public boolean exist(long id) {
+		return repository.existsById(id);
+	}
+
+	public List<OrderItem> findAll() {
+		return repository.findAll();
+	}
+
+    public void save(OrderItem orderItem) {
+        repository.save(orderItem);
+    }
+
+	public void delete(long id) {
+		repository.deleteById(id);
+	}
+}
