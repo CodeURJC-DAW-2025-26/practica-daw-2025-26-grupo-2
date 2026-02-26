f153fd0c64f09ace131cc1021d6295a9bfcca3c9
SamuelMelian <s.melian.2022@alumnos.urjc.es>
2026-02-26 09:13:19 +0100

    fix: remove unnecesary import

--- backend/src/main/java/es/dawgrupo2/zendashop/security/WebSecurityConfig.java
+++ backend/src/main/java/es/dawgrupo2/zendashop/security/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package es.dawgrupo2.zendashop.security;
 
 import org.springframework.context.annotation.Bean;
-import es.dawgrupo2.zendashop.controller.ErrorPageController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
