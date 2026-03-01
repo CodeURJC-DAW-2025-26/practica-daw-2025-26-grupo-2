c25161ee40a081d4f46c4f1d86abace0042332c8
saraguillenmtz <s.guillenm.2022@alumnos.urjc.es>
2026-03-01 18:18:50 +0100

    feat: delete

--- backend/src/main/java/es/dawgrupo2/zendashop/model/Order.java
+++ backend/src/main/java/es/dawgrupo2/zendashop/model/Order.java
@@ -33,6 +33,7 @@ public class Order {
     private BigDecimal subtotal;
     private BigDecimal shippingCost;
     private BigDecimal totalPrice;
+  
 
     @CreationTimestamp
     @Column(updatable = false)
