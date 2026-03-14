e67cad3ad19f32e8c7e58c96b39acd30cf70cb2f
SamuelMelian <s.melian.2022@alumnos.urjc.es>
2026-03-14 19:38:44 +0100

    feat: Add OpinionBasicDTO and update OpinionDTO to include user and garment references

---
+++ backend/src/main/java/es/dawgrupo2/zendashop/dto/OpinionBasicDTO.java
@@ -0,0 +1,9 @@
+package es.dawgrupo2.zendashop.dto;
+
+public record OpinionBasicDTO(
+    Long id,
+    String comment,
+    Integer rating
+) {
+    
+}
--- backend/src/main/java/es/dawgrupo2/zendashop/dto/OpinionDTO.java
+++ backend/src/main/java/es/dawgrupo2/zendashop/dto/OpinionDTO.java
@@ -3,7 +3,8 @@ package es.dawgrupo2.zendashop.dto;
 public record OpinionDTO(
     Long id,
     String comment,
-    Integer rating
+    Integer rating,
+    UserBasicDTO user,
+    GarmentBasicDTO garment
 ) {
-    
 }
