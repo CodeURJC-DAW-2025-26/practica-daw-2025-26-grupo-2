060a42daa23663d4b18e9b57031cbdcaaba86986
SamuelMelian <s.melian.2022@alumnos.urjc.es>
2026-03-16 23:35:13 +0100

    chore: Add TODOs for organizing DTOs and implementing Statistics DTOs in GarmentBasicDTO

--- backend/src/main/java/es/dawgrupo2/zendashop/dto/GarmentBasicDTO.java
+++ backend/src/main/java/es/dawgrupo2/zendashop/dto/GarmentBasicDTO.java
@@ -9,6 +9,8 @@ public record GarmentBasicDTO (
     String category,
     String description,
     String features
+    // TODO: Ask about including folders for mappers and dtos, as there are many dtos and mappers, and it would be more organized
+    // TODO: Implement DTOs for Statistics, ask about how could we do it, as we need to return some specific data for the statistics
 ) {
     
 }
