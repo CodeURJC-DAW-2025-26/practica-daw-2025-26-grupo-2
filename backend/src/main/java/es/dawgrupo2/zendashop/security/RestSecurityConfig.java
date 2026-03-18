a4160321c4afe0d1f867e170e1bb273552bd4332
SamuelMelian <s.melian.2022@alumnos.urjc.es>
2026-03-18 18:28:59 +0100

    feat: Update RestSecurityConfig to enhance API security; restructure access rules and endpoints for better organization

--- backend/src/main/java/es/dawgrupo2/zendashop/security/RestSecurityConfig.java
+++ backend/src/main/java/es/dawgrupo2/zendashop/security/RestSecurityConfig.java
@@ -57,28 +57,29 @@ public class RestSecurityConfig {
         http.authorizeHttpRequests(authorize -> authorize
             // Public Access
             .requestMatchers("/css/**", "/js/**", "/images/**", "/static/**", "/*.css", "/*.js", "/*.png").permitAll()
-            .requestMatchers(HttpMethod.GET, "/", "/api/*/garments/**", "/api/*/garments/*/image", "/api/users/*/avatar", "/api/*/images/**").permitAll()
-            .requestMatchers(HttpMethod.POST, "/api/*/auth/login", "/api/*/auth/refresh", "/api/users/register").permitAll()
+            .requestMatchers(HttpMethod.GET, "/", "/api/v1/garments/*", "/api/v1/garments/*/image", "/api/v1/images/*/media").permitAll()
+            .requestMatchers(HttpMethod.POST, "/api/v1/auth/login", "/api/v1/auth/refresh", "/api/v1/users/register").permitAll()
                 
             // Users Access
-            .requestMatchers(HttpMethod.GET, "/api/myorders", "/api/cart/**").hasAnyRole("USER", "ADMIN")
-            .requestMatchers(HttpMethod.POST, "/api/garments/*/opinions", "/api/orders").hasAnyRole("USER", "ADMIN")
-            .requestMatchers(HttpMethod.PUT, "/api/users/profile", "/api/opinions/*").hasAnyRole("USER", "ADMIN")
-            .requestMatchers(HttpMethod.DELETE, "/api/opinions/*").hasAnyRole("USER", "ADMIN")
+            .requestMatchers(HttpMethod.GET, "/api/v1/me/orders", "/api/v1/me", "/api/v1/me/cart",  "/api/v1/users/*/image", "/api/v1/orders/*/orderItems/", "/api/v1/orders/*/orderItems/*", "/api/v1/orders/*").hasAnyRole("USER", "ADMIN")
+            .requestMatchers(HttpMethod.POST, "/api/v1/garments/*/opinions/", "/api/v1/orders/*", "/api/v1/orders/*/orderItems/*").hasAnyRole("USER", "ADMIN")
+            .requestMatchers(HttpMethod.PUT, "/api/v1/me", "/api/v1/opinions/*", "/api/v1/orders/*/orderItems/*").hasAnyRole("USER", "ADMIN")
+            .requestMatchers(HttpMethod.DELETE, "/api/v1/opinions/*", "/api/v1/orders/*", "/api/v1/orders/*/orderItems/*").hasAnyRole("USER", "ADMIN")
 
             // Admins Access
-            .requestMatchers(HttpMethod.POST, "/api/garments").hasRole("ADMIN")
-            .requestMatchers(HttpMethod.PUT, "/api/garments/*", "/api/orders/*").hasRole("ADMIN")
-            .requestMatchers(HttpMethod.DELETE, "/api/garments/*", "/api/orders/*").hasRole("ADMIN")
-            .requestMatchers(HttpMethod.GET, "/api/users", "/api/statistics", "/api/orders").hasRole("ADMIN")
+            .requestMatchers(HttpMethod.POST, "/api/v1/garments").hasRole("ADMIN")
+            .requestMatchers(HttpMethod.PUT, "/api/v1/garments/*", "/api/v1/orders/*").hasRole("ADMIN")
+            .requestMatchers(HttpMethod.DELETE, "/api/v1/garments/*", "/api/v1/orders/*").hasRole("ADMIN")
+            .requestMatchers(HttpMethod.GET, "/api/v1/users/", "/api/v1/statistics/", "/api/v1/orders/").hasRole("ADMIN")
 
-            .anyRequest().authenticated()
+            .anyRequest().permitAll()
         );
         http.formLogin(formLogin -> formLogin.disable());
         http.csrf(csrf -> csrf.disable());
         http.httpBasic(basic -> basic.disable());
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        http.exceptionHandling(ex -> ex.authenticationEntryPoint(unauthorizedHandlerJwt));
 
         return http.build();
     }
