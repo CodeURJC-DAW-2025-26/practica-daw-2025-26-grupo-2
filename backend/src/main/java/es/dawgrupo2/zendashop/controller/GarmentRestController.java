697853b970a761819fe1771348e13c332be5005e
Jorge Padilla <jorge.padillarodriguez@uzh.ch>
2026-03-20 18:34:26 +0100

    fixing offers access

--- backend/src/main/java/es/dawgrupo2/zendashop/controller/GarmentRestController.java
+++ backend/src/main/java/es/dawgrupo2/zendashop/controller/GarmentRestController.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -84,6 +85,8 @@ public class GarmentRestController {
             if (userOpt.isPresent()) {
                 garments = garmentService.findSmartRecommendations(userOpt.get().getId());
             }
+        } else {
+            throw new AccessDeniedException("Tienes que estar loggeado para obtener recomendaciones");
         }
         return garmentBasicMapper.toDTOs(garments);
     }
