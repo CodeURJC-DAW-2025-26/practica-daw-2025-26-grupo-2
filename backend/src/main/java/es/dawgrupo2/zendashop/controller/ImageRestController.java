2b2b8d9768b95994d16278467e642d2ff8968f87
Jorge Padilla <jorge.padillarodriguez@uzh.ch>
2026-03-22 11:05:46 +0100

    control de seguridad en la edición de una imagen

--- backend/src/main/java/es/dawgrupo2/zendashop/controller/ImageRestController.java
+++ backend/src/main/java/es/dawgrupo2/zendashop/controller/ImageRestController.java
@@ -2,12 +2,14 @@ package es.dawgrupo2.zendashop.controller;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.MediaTypeFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -16,9 +18,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 import es.dawgrupo2.zendashop.basicDTO.ImageDTO;
 import es.dawgrupo2.zendashop.basicDTO.ImageMapper;
+import es.dawgrupo2.zendashop.model.User;
 import es.dawgrupo2.zendashop.service.ImageService;
+import es.dawgrupo2.zendashop.service.UserService;
 
 @RestController
 @RequestMapping("/api/v1/images")
@@ -26,6 +32,9 @@ public class ImageRestController {
 
     @Autowired
     private ImageService imageService;
+
+    @Autowired
+    private UserService userService;
 
     @Autowired
     private ImageMapper mapper;
@@ -53,8 +62,12 @@ public class ImageRestController {
 
     @PutMapping("/{id}/media")
     public ResponseEntity<Object> replaceImageFile(@PathVariable long id,
-            @RequestParam MultipartFile imageFile) throws IOException {
-
+            @RequestParam MultipartFile imageFile, HttpServletRequest request) throws IOException {
+        User user = userService.findByEmail(request.getUserPrincipal().getName()).orElseThrow(() -> new NoSuchElementException("Usuario no encontrado"));
+        if (!request.isUserInRole("ADMIN") && (user.getAvatar() == null || !user.getAvatar().getId().equals(id))) {
+			throw new AccessDeniedException("No tienes permisos de edición de imágenes aquí");
+		}
+        
         imageService.replaceImageFile(id, imageFile.getInputStream());
         return ResponseEntity.noContent().build();
     }
