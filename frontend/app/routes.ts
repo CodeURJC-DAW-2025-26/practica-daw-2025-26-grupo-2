9c501079cd5bb6ad82b6365cf1ca311222322036
SamuelMelian <s.melian.2022@alumnos.urjc.es>
2026-04-16 12:00:10 +0200

    feat: update routing configuration to include user and order routes

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,3 +1,25 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  route("login", "routes/login.tsx"),
+  route("register", "routes/user-new.tsx"),
+
+  layout("routes/home.tsx", [
+    index("routes/garment-list.tsx"),
+    route("garment/new", "routes/garment-new.tsx"),
+    route("garment/:id", "routes/garment-detail.tsx"),
+    route("garment/:id/edit", "routes/garment-edit.tsx"),
+
+    route("orders", "routes/order-list.tsx"),
+    route("orders/:id", "routes/order-detail.tsx"),
+    route("orders/:id/edit", "routes/order-edit.tsx"),
+    route("cart", "routes/cart.tsx"),
+
+    route("users", "routes/user-list.tsx"),
+    route("users/new", "routes/user-new.tsx"),
+    route("users/:id", "routes/user-detail.tsx"),
+    route("users/:id/edit", "routes/user-edit.tsx"),
+  ]),
+
+  route("*", "routes/not-found.tsx"),
+] satisfies RouteConfig;
